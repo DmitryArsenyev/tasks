20dcc374ea052c0cb77d4dfccd1646b0a163510b
darsenev <dima.arsenjev@mail.ru>
2022-06-23 00:19:38 +0300

    Деление

--- first/nums.py
+++ first/nums.py
@@ -7,7 +7,12 @@ def mul(a,b):
 def minus(a,b):
     return a - b
 
+def delit(a,b):
+    return a / b
+
+
 if __name__ == '__main__':
      print(plus(2, 2))
      print(minus(4, 2))
      print(mul(3, 3))
+     print(delit(8,2))
