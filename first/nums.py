bd3f63ac07f6db73bb4a5c91e34ef7d4f1d4fea7
darsenev <dima.arsenjev@mail.ru>
2022-07-04 21:30:34 +0300

    Полиморфизм

--- first/nums.py
+++ first/nums.py
@@ -12,6 +12,35 @@ def delit(a,b):
 
 print(2)
 
+class Mammal:
+    def move(self):
+        print('Двигается')
+
+class Hare(Mammal):
+    def move(self):
+        print('Прыгает')
+
+animal = Mammal()
+animal.move() # Двигается
+hare = Hare()
+hare.move()
+
+
+class Parent():
+    def __init__(self):
+        print('Parent init')
+
+    def method(self):
+        print('Parent method')
+
+class Child(Parent):
+    def __init__(self):
+        Parent.__init__(self)
+
+    def method(self):
+        super(Child, self).method()
+
+child = Child()
 
 if __name__ == '__main__':
      print(plus(2, 2))
