06d41001281b990076a17b91a0b6a0e12ee55487
darsenev <dima.arsenjev@mail.ru>
2022-07-20 21:23:26 +0300

    Пример композиции

---
+++ first/composition.py
@@ -0,0 +1,43 @@
+class Salary:
+    def __init__(self,pay):
+        self.pay = pay
+
+    def getTotal(self):
+        return (self.pay*12)
+
+class Employee:
+    def __init__(self,pay,bonus):
+        self.pay = pay
+        self.bonus = bonus
+        self.salary = Salary(self.pay)
+
+    def annualSalary(self):
+        return "Total: " + str(self.salary.getTotal() + self.bonus)
+
+
+
+
+
+
+
+
+
+class Salary(object):
+    def __init__(self, pay):
+        self.pay = pay
+
+    def getTotal(self):
+        return (self.pay * 12)
+
+class Employee(object):
+    def __init__(self, pay, bonus):
+        self.pay = pay
+        self.bonus = bonus
+
+    def annualSalary(self):
+        return "Total: " + str(self.pay.getTotal() + self.bonus)
+
+salary = Salary(100)
+employee = Employee(salary, 10)
+print(employee.annualSalary())
+
