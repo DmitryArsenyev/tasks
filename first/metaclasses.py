3d002b52a9bd6481ab9beaa0f82f3ddff5973eae
darsenev <dima.arsenjev@mail.ru>
2022-07-31 21:34:23 +0300

    Метаклассы

--- first/metaclasses.py
+++ first/metaclasses.py
@@ -21,3 +21,21 @@ class Child(SomeClass):
 
 # получение экземпляра класса
 obj = Child("Hello")
+
+
+
+class UpperAttrMetaclass(type):
+
+    def __new__(cls, clsname, bases, dct):
+
+        uppercase_attr = {}
+        for name, val in dct.items():
+            if not name.startswith('__'):
+                uppercase_attr[name.upper()] = val
+            else:
+                uppercase_attr[name] = val
+
+        return type.__new__(cls, clsname, bases, uppercase_attr)
+
+
+
